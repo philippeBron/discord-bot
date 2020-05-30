9900b345ea1fff83f8f291f31428e7f024bd3f24
philippeBron <philippe.bron@gmail.com>
2020-05-30 22:17:00 +0200

    ✨ add response

--- index.js
+++ index.js
@@ -4,4 +4,16 @@ const bot = new Discord.Client();
 
 const TOKEN = process.env.BOT_TOKEN;
 
+bot.on('ready', function () {
+    console.log("Je suis connecté !")
+})
+
+bot.on('message', message => {
+    if (message.content === 'ping') {
+      message.reply('pong !')
+    } else if (message.content === 'lol') {
+        message.reply('ptdr')
+    }
+})
+
 bot.login(TOKEN);

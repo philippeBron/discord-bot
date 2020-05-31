9a1d23b66d68bdbd2759cb7a665f8a60f7a57464
philippeBron <philippe.bron@gmail.com>
2020-05-31 11:50:00 +0200

    :wip: test

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ bot.on('message', message => {
     if (message.content.toUpperCase() === 'PING') {
       message.reply('pong !')
     } else if (message.content.toUpperCase()=== 'LOL') {
-        message.reply('ptdr!!!')
+        message.reply('ptdr!')
     }
 })
 

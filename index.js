9a4fc6dfc7abbe86999c7f8f41a37863e636ac9c
philippeBron <philippe.bron@gmail.com>
2020-05-31 13:38:40 +0200

    💄 add  emoji

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ bot.on('message', message => {
     if (message.content.toUpperCase() === 'PING') {
       message.reply('pong !')
     } else if (message.content.toUpperCase()=== 'LOL') {
-        message.reply('ptdr!')
+        message.reply('ptdr ! :rofl:')
     } else if (message.content.toUpperCase()=== 'KKBOUDIN') {
         message.reply(':poop:')
     }
